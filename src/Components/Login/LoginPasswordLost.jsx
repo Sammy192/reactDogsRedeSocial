cb8a9c18368b9d924237d24074456a27e8047e6c
Samuel R Borges <sammy_rb@hotmail.com>
2023-12-08 08:31:26 -0300

    config try

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -25,7 +25,7 @@ const LoginPasswordLost = () => {
   }
 
   return (
-    <section className="animeLeft">
+    <section>
       <Head title="Perdeu a senha?" />
       <h1 className="title">Perdeu a senha?</h1>
       {data ? (
