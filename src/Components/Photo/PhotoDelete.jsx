c9b58192c1a41193dd45acd8b192c3e8e18d2258
Samuel R Borges <sammy_rb@hotmail.com>
2023-12-10 09:03:47 -0300

    config

--- src/Components/Photo/PhotoDelete.jsx
+++ src/Components/Photo/PhotoDelete.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import styles from './PhotoDelete.module.css';
 import useFetch from '../../Hooks/useFetch';
 import { PHOTO_DELETE } from '../../Api';
+import { useNavigate } from 'react-router-dom';
 
 const PhotoDelete = ({ id }) => {
   const { loading, request } = useFetch();
+  const navigate = useNavigate();
 
   async function handleClick() {
     const cofirm = window.confirm('Tem certeza que deseja deletar?');
     if (confirm) {
       const { url, options } = PHOTO_DELETE(id);
       const { response } = await request(url, options);
-      if (response.ok) window.location.reload();
+      if (response.ok) navigate('/conta/postar');
     }
   }
 
